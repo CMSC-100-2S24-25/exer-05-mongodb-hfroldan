b65e79dbe74c4e055b91270905fac3eae218cc00
hfroldan <hfroldan@up.edu.ph>
2025-03-12 19:37:39 +0800

    feat: add corresponding controller functions

--- controller.js
+++ controller.js
@@ -36,3 +36,119 @@ const studentSchema = new mongoose.Schema({
 })
 
 const Student = mongoose.model('Student', studentSchema);
+
+// --------------------------------
+// Controller Functions
+// --------------------------------
+
+// @desc    Add a new student
+// @route   POST /save-student
+// @access  Public
+const saveStudent = async (req, res) => {
+    try {
+        const { stdnum } = req.body;
+        const studentExists = await Student.findOne({ stdnum });
+
+        if (studentExists) {
+            return res.status(400).json({inserted: false, message: 'Student already exists. Cannot insert a new record.'});
+        }
+
+        const student = await new Student(req.body).save();
+        return res.status(201).json({inserted: true, student});
+    } catch (error) {
+        return res.status(500).json({inserted: false});
+    }
+}
+
+// @desc    Update a student
+// @route   POST /update
+// @access  Public
+const updateStudent = async (req, res) => {
+    try {
+        const { fname, updatedfname, updatedlname } = req.body;
+        const student = await Student.findOne({ fname });
+
+        if (!student) {
+            return res.status(404).json({updated: false, message: 'Student not found. Cannot update record.'})
+        }
+
+        const updatedStudent = await Student.findOneAndUpdate(
+            {fname: fname}, 
+            {fname: updatedfname, lname: updatedlname}, 
+            {new: true}
+        );
+
+        return res.status(200).json({updated: true, updatedStudent});
+    } catch (error) {
+        return res.status(500).json({updated: false});
+    }
+}
+
+// @desc    Delete a student
+// @route   POST /remove-user
+// @access  Public
+const removeStudent = async (req, res) => {
+    try {
+        const { stdnum } = req.body;
+        const student = await Student.findOne({stdnum});
+
+        if (!student) {
+            return res.status(404).json({updated: false, message: 'Student not found. Cannot delete record.'})
+        }
+
+        const deletedStudent = await Student.deleteOne({stdnum});
+        return res.status(200).json({deleted: true, deletedStudent});
+    } catch (error) {
+        return res.status(500).json({deleted: false});
+    }
+}
+
+// @desc    Delete all students
+// @route   POST /remove-all-user
+// @access  Public
+const removeAllStudents = async (req, res) => {
+    try {
+        const result = await Student.deleteMany({});
+
+        return res.status(200).json({deleted: true, result});
+    } catch (error) {
+        return res.status(500).json({deleted: false});
+    }
+}
+
+// @desc    Find a student by their stdnum
+// @route   GET /user
+// @access  Public
+const getStudent = async (req, res) => {
+    const { stdnum } = req.query;
+    if (!stdnum) { 
+        return res.status(400).json({message: 'Student number is required', data: []}); 
+    }
+
+    try {
+        const student = await Student.find({stdnum});
+        return res.status(200).json(student || {message: 'Student not found.'});
+    } catch (error) {
+        res.status(500).json([]);
+    }
+}
+
+// @desc    Find all students
+// @route   GET /members
+// @access  Public
+const getAllStudents = async (req, res) => {
+    try {
+        return res.status(200).json(await Student.find({}));
+    } catch (error) {
+        res.status(500).json([]);
+    }
+}
+
+export {
+    saveStudent,
+    updateStudent,
+    removeStudent,
+    removeAllStudents,
+    getStudent,
+    getAllStudents
+}
